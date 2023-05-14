2e1a2d14f005ed376a71a7ac51060e18416d96d0
Joe Polny <joepolny@gmail.com>
2023-05-13 20:33:37 -0400

    proposal tuple2 key

--- session_2/app.py
+++ session_2/app.py
@@ -13,10 +13,16 @@ class DAOState:
 
     # Box Storage
     proposals = BoxMapping(
-        key_type=abi.Address, value_type=abi.String, prefix=Bytes("p-")
+        key_type=abi.Tuple2[abi.Address, abi.Uint64],
+        value_type=abi.String,
+        prefix=Bytes("p-"),
     )
 
-    votes = BoxMapping(key_type=abi.Address, value_type=abi.Uint64, prefix=Bytes("v-"))
+    votes = BoxMapping(
+        key_type=abi.Tuple2[abi.Address, abi.Uint64],
+        value_type=abi.Uint64,
+        prefix=Bytes("v-"),
+    )
 
     has_voted = BoxMapping(
         key_type=abi.Address, value_type=abi.Bool, prefix=Bytes("h-")
@@ -32,33 +38,41 @@ def create() -> Expr:
 
 
 @app.external
-def add_proposal(desc: abi.String) -> Expr:
+def add_proposal(desc: abi.String, proposal_id: abi.Uint64) -> Expr:
+    proposal_key = abi.make(abi.Tuple2[abi.Address, abi.Uint64])
+    addr = abi.Address()
+
     return Seq(
+        addr.set(Txn.sender()),
+        proposal_key.set(addr, proposal_id),
         # Check if the proposal already exists
-        Assert(app.state.proposals[Txn.sender()].exists() == Int(0)),
+        Assert(app.state.proposals[proposal_key].exists() == Int(0)),
         # Not using .get() here because desc is already a abi.String
         app.state.proposals[Txn.sender()].set(desc),
     )
 
 
 @app.external
-def support(proposer: abi.Address) -> Expr:
+def support(proposer: abi.Address, proposal_id: abi.Uint64) -> Expr:
     total_votes = abi.Uint64()
     current_votes = abi.Uint64()
     true_value = abi.Bool()
+    proposal_key = abi.make(abi.Tuple2[abi.Address, abi.Uint64])
+
 
     return Seq(
+        proposal_key.set(proposer, proposal_id),
         # Make sure we haven't voted yet
         Assert(app.state.proposals[Txn.sender()].exists() == Int(0)),
         # Get current vote count
-        app.state.votes[proposer].store_into(current_votes),
+        app.state.votes[proposal_key].store_into(current_votes),
         # Increment and save total vote count
         total_votes.set(current_votes.get() + Int(1)),
-        app.state.votes[proposer].set(total_votes),
+        app.state.votes[proposal_key].set(total_votes),
         # Check if this proposal is now winning
         If(total_votes.get() > app.state.winning_proposal_votes.get()).Then(
             app.state.winning_proposal_votes.set(total_votes.get()),
-            app.state.winning_proposal.set(proposer.get()),
+            app.state.winning_proposal.set(proposal_key.encode()),
         ),
         # Set has_voted to true
         true_value.set(True),
